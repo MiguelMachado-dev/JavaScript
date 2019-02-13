235108794c8575a1c1075212fba0117f38d69959
Miguel Machado <miguel.machado@agenciamizu.com.br>
2019-02-13 21:22:52 -0200

    and, or, not

---
+++ RocketSeat - JavaScript/1.7 - operadores logicos.js
@@ -0,0 +1,17 @@
+var sexo = "m",
+  idade = 21;
+
+if (sexo === "m" && idade >= 18) {
+  console.log("sexo e idade sao true. Precisam ser true");
+}
+
+if (sexo === "m" || idade <= 18) {
+  console.log("sexo ou idade sao true");
+}
+
+if (sexo !== "f") {
+  console.log("Sexo nao é F... True");
+}
+// if(!(sexo == true)) {
+//  console.log('Sexo nao é F... True')
+// }
